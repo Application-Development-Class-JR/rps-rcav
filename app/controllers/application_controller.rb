41584017f7d12f84a8073840269c942b80b84f7b
juliannaresende <jresende@chicagobooth.edu>
2022-04-21 16:22:07 +0000

    rock_page_working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    #write code
+    #redirect_to("https://www.wikipedia.org")
+    #render({:plain=>"Hello, World!"})
+    #render({:plain=>rand(100)})
+    #render({:html=>"<h1>Hello, world!</h1>".html_safe})
+    render({:template=>"game_templates/user_rock.html.erb"})
+  end
 end
